9e07928a0739e7555406c7a00135b1e1065a7220
bcskda <demulkup@ya.ru>
2021-03-15 12:29:54 +0300

    [hometask-1] Application configuration -> main

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -4,57 +4,42 @@ import ru.sbt.mipt.oop.commands.handlers.LightOffCommandHandler;
 import ru.sbt.mipt.oop.commands.handlers.LogCommandHandler;
 import ru.sbt.mipt.oop.commands.handlers.SensorCommandHandler;
 import ru.sbt.mipt.oop.events.handlers.*;
-import ru.sbt.mipt.oop.events.sources.SensorEventSource;
 import ru.sbt.mipt.oop.events.sources.SensorEventSourceStub;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class Application {
     public static String DEFAULT_CONF_PATH = "smart-home-1.json";
 
-    private final SmartHome smartHome;
-    private final CommandSender commandSender;
-    private final SensorEventLoop eventLoop;
-
     public static void main(String[] args) {
-        Application app;
+        SensorEventLoop eventLoop;
         try {
-            app = new Application(args);
+            // считываем состояние дома из файла
+            String filename = (args.length > 0) ? args[0] : DEFAULT_CONF_PATH;
+            SmartHome smartHome = new JsonConfigurationReader(Paths.get(filename)).readSmartHome();
+
+            // обработчики и контроллер команд
+            CommandSender commandSender = new CommandSenderImpl(makeCommandHandlers(smartHome));
+
+            // обработчики событий
+            eventLoop = new SensorEventLoop(
+                    new SensorEventSourceStub(),
+                    makeEventHandlers(smartHome, commandSender));
         } catch (Exception e) {
             throw new IllegalArgumentException("Failed to configure Application", e);
         }
-        app.run();
-    }
-
-    Application(String[] args) throws IOException {
-        // считываем состояние дома из файла
-        String filename = (args.length > 0) ? args[0] : DEFAULT_CONF_PATH;
-        smartHome = new JsonConfigurationReader(Paths.get(filename)).readSmartHome();
-
-        // обработчики команд и контроллер
-        List<SensorCommandHandler> commandHandlers = initCommandHandlers();
-        commandSender = new CommandSenderImpl(commandHandlers);
-
-        // создаём обработчики событий
-        SensorEventSource eventSource = new SensorEventSourceStub();
-        List<SensorEventHandler> eventHandlers = initEventHandlers();
-        eventLoop = new SensorEventLoop(eventSource, eventHandlers);
-    }
-
-    public void run() {
         eventLoop.runCatchSuppress();
     }
 
-    private List<SensorCommandHandler> initCommandHandlers() {
+    private static List<SensorCommandHandler> makeCommandHandlers(SmartHome smartHome) {
         List<SensorCommandHandler> handlers = new ArrayList<>();
         handlers.add(new LogCommandHandler());
         handlers.add(new LightOffCommandHandler(smartHome));
         return handlers;
     }
 
-    private List<SensorEventHandler> initEventHandlers() {
+    private static List<SensorEventHandler> makeEventHandlers(SmartHome smartHome, CommandSender commandSender) {
         List<SensorEventHandler> handlers = new ArrayList<>();
         handlers.add(new LogEventHandler());
 
