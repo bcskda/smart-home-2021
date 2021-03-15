0f6ea6a7538279d55b40e6b69e2337d7573aef28
bcskda <demulkup@ya.ru>
2021-03-15 14:05:54 +0300

    [hometask-2] Cleaner HomeBuilder

--- src/main/java/ru/sbt/mipt/oop/HomeBuilder.java
+++ src/main/java/ru/sbt/mipt/oop/HomeBuilder.java
@@ -8,41 +8,44 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class HomeBuilder {
 
     public static void main(String[] args) throws IOException {
-        Room kitchen = new Room(new ArrayList<>(), new ArrayList<>(), "kitchen");
-        kitchen.getLights().addAll(Arrays.asList(
-                new Light("1", false),
-                new Light("2", true)));
-        kitchen.getDoors().addAll(Collections.singletonList(
-                new Door("1", false)));
+        Room kitchen = new Room(
+                Arrays.asList(
+                        new Light("1", false),
+                        new Light("2", true)),
+                Collections.singletonList(
+                        new Door("1", false)),
+                "kitchen");
 
-        Room bathroom = new Room(new ArrayList<>(), new ArrayList<>(), "bathroom");
-        bathroom.getLights().addAll(Collections.singletonList(
-                new Light("3", true)));
-        bathroom.getDoors().addAll(Collections.singletonList(
-                new Door("2", false)));
+        Room bathroom = new Room(
+                Collections.singletonList(
+                        new Light("3", true)),
+                Collections.singletonList(
+                        new Door("2", false)),
+                "bathroom");
 
-        Room bedroom = new Room(new ArrayList<>(), new ArrayList<>(), "bedroom");
-        bedroom.getLights().addAll(Arrays.asList(
-                new Light("4", false),
-                new Light("5", false),
-                new Light("6", false)));
-        bedroom.getDoors().addAll(Collections.singletonList(
-                new Door("3",  true)));
+        Room bedroom = new Room(
+                Arrays.asList(
+                        new Light("4", false),
+                        new Light("5", false),
+                        new Light("6", false)),
+                Collections.singletonList(
+                        new Door("3", true)),
+                "bedroom");
 
-        Room hall = new Room(new ArrayList<>(), new ArrayList<>(), "hall");
-        hall.getLights().addAll(Arrays.asList(
-                new Light("7", false),
-                new Light("8", false),
-                new Light("9", false)));
-        hall.getDoors().addAll(Collections.singletonList(
-                new Door("4", false)));
+        Room hall = new Room(
+                Arrays.asList(
+                        new Light("7", false),
+                        new Light("8", false),
+                        new Light("9", false)),
+                Collections.singletonList(
+                        new Door("4", false)),
+                "hall");
 
         SmartHome smartHome = new SmartHome(Arrays.asList(kitchen, bathroom, bedroom, hall));
         Gson gson = new GsonBuilder()
