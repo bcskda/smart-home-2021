64a7ebe8f34673038b2c97d617b949f7316c23b7
bcskda <demulkup@ya.ru>
2021-03-22 14:29:43 +0300

    [hometask-3] AlarmSensorEvent ctor checks event type

--- src/main/java/ru/sbt/mipt/oop/events/AlarmSensorEvent.java
+++ src/main/java/ru/sbt/mipt/oop/events/AlarmSensorEvent.java
@@ -5,6 +5,18 @@ public class AlarmSensorEvent extends SensorEvent {
 
     public AlarmSensorEvent(SensorEventType type, String objectId, String code) {
         super(type, objectId);
+        switch (type) {
+            case ALARM_ACTIVATE:
+            case ALARM_DEACTIVATE:
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        "Invalid event type for AlarmSensorEvent: " + type);
+        }
         this.code = code;
+    }
+
+    public String getCode() {
+        return code;
     }
 }
