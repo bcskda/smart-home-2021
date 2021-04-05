69e03499922b8fd75fe18ccdbe46324a90a81043
Dmitrii Kuptsov <demulkup@ya.ru>
2021-04-05 13:25:45 +0300

    [hometask-4] Better EventHandlerAdaptor

--- src/main/java/ru/sbt/mipt/oop/ccadapt/EventHandlerAdaptor.java
+++ src/main/java/ru/sbt/mipt/oop/ccadapt/EventHandlerAdaptor.java
@@ -3,22 +3,37 @@ package ru.sbt.mipt.oop.ccadapt;
 import com.coolcompany.smarthome.events.CCSensorEvent;
 import ru.sbt.mipt.oop.Action;
 import ru.sbt.mipt.oop.events.Event;
+import ru.sbt.mipt.oop.events.EventType;
+import ru.sbt.mipt.oop.events.SensorEvent;
 import ru.sbt.mipt.oop.events.handlers.EventHandler;
 
+import java.util.Locale;
 import java.util.function.Consumer;
 
 public class EventHandlerAdaptor implements com.coolcompany.smarthome.events.EventHandler {
-    EventHandler actual;
-    Consumer<Action> actionConsumer;
+    private final EventHandler nativeHandler;
+    private final Consumer<Action> actionConsumer;
 
-    public EventHandlerAdaptor(EventHandler actual, Consumer<Action> actionConsumer) {
-        this.actual = actual;
+    public EventHandlerAdaptor(EventHandler nativeHandler, Consumer<Action> actionConsumer) {
+        this.nativeHandler = nativeHandler;
         this.actionConsumer = actionConsumer;
     }
 
     @Override
     public void handleEvent(CCSensorEvent foreignEvent) {
-        Event event = EventAdaptor.adapt(foreignEvent);
-        actionConsumer.accept(actual.handleEvent(event));
+        Event event = adaptEvent(foreignEvent);
+        actionConsumer.accept(nativeHandler.handleEvent(event));
+    }
+
+    private static Event adaptEvent(CCSensorEvent event) {
+        EventType eventType = adaptEventType(event.getEventType());
+        return new SensorEvent(eventType, event.getObjectId());
+    }
+
+    private static EventType adaptEventType(String ccEventType) {
+        // Для данной версии либы работает
+        return EventType.valueOf(ccEventType
+                .toUpperCase(Locale.ROOT)
+                .replace("IS", "_"));
     }
 }
