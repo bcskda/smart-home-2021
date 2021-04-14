d17cdc1858f76b037886644aedc3ea9f777dd780
bcskda <demulkup@ya.ru>
2021-04-14 21:34:18 +0300

    [hometask-4] Better CC event type translation

--- src/main/java/ru/sbt/mipt/oop/ccadapt/EventHandlerAdaptor.java
+++ src/main/java/ru/sbt/mipt/oop/ccadapt/EventHandlerAdaptor.java
@@ -8,12 +8,22 @@ import ru.sbt.mipt.oop.events.EventType;
 import ru.sbt.mipt.oop.events.SensorEvent;
 import ru.sbt.mipt.oop.events.handlers.EventHandler;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class EventHandlerAdaptor implements com.coolcompany.smarthome.events.EventHandler {
+    private static final Map<String, EventType> eventTypeMap = new HashMap<>();
     private final EventHandler nativeHandler;
     private final SmartHome smartHome;
+
+    static {
+        eventTypeMap.put("LightIsOn", EventType.LIGHT_ON);
+        eventTypeMap.put("LightIsOff", EventType.LIGHT_OFF);
+        eventTypeMap.put("DoorIsOpen", EventType.DOOR_OPEN);
+        eventTypeMap.put("DoorIsClosed", EventType.DOOR_CLOSED);
+    }
 
     public EventHandlerAdaptor(EventHandler nativeHandler, SmartHome smartHome) {
         this.nativeHandler = nativeHandler;
@@ -23,20 +33,23 @@ public class EventHandlerAdaptor implements com.coolcompany.smarthome.events.Eve
     @Override
     public void handleEvent(CCSensorEvent foreignEvent) {
         Event event = adaptEvent(foreignEvent);
+        if (event == null)
+            return;
         Action action = nativeHandler.handleEvent(event);
-        if (action != null)
-            smartHome.execute(action);
+        if (action == null)
+            return;
+        smartHome.execute(action);
     }
 
     private static Event adaptEvent(CCSensorEvent event) {
         EventType eventType = adaptEventType(event.getEventType());
-        return new SensorEvent(eventType, event.getObjectId());
+        if (eventType == null)
+            return null;
+        else
+            return new SensorEvent(eventType, event.getObjectId());
     }
 
     private static EventType adaptEventType(String ccEventType) {
-        // Для данной версии либы работает
-        return EventType.valueOf(ccEventType
-                .toUpperCase(Locale.ROOT)
-                .replace("IS", "_"));
+        return eventTypeMap.get(ccEventType);
     }
 }
